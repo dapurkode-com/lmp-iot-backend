dfda792fbc9cdbfd89230d433900098c2808071c
I Gusti Bagus Ngurah Satya Wibawa <i.g.b.n.satyawibawa@gmail.com>
2021-06-02 15:42:20 +0800

    disable bg process

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,8 +26,8 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
 
-        $schedule->command('mygooglefit:fetch')->everyMinute();
-        $schedule->command('ramz:notify')->dailyAt('08:00');
+        // $schedule->command('mygooglefit:fetch')->everyMinute();
+        // $schedule->command('ramz:notify')->dailyAt('08:00');
     }
 
     /**
